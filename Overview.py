c28bb5907b01b0589dea5f9733b6416c2def9759
liz-aws <66441437+liz-aws@users.noreply.github.com>
2020-06-04 15:42:55 -0500

    Updated with background

---
+++ Overview.py
@@ -0,0 +1,31 @@
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# Test notebook
+# # 1
+# ##  2
+# 2
+#
+# Bold *text* **two**
+
+#
+# ### Background
+# 
+# Data engineering and systems architecture is a necessary precursor to delivering data-driven subsurface insights. As a geologist or geophysicist, systems design typically isn’t something you learn in school, yet it can be a necessary skill set to progress your career and drive meaningful change.
+#
+#
+# ### Session Overview
+# This session will present an overview of the key concepts and design principles for building a scalable and cloud-optimized infrastructure in line with the AWS Well-Architected Framework, with a specific focus on Energy-Industry use cases and accompanying challenges for subsurface professionals. Attendees will leave this session with techniques for outlining technical requirements, knowledge of key AWS services and solutions, an overview of the AWS Well-Architected Framework, and an understanding of how to design for scalable and resilient solutions that can be implemented to improve subsurface workflows.
+#
+# ### Agenda
+# Working backwards: User stories to drive technical requirements
+# Services, solutions, and scalability
+# Whiteboarding as a superpower
+# The AWS Well-Architected Framework
+# Now what? How to communicate and drive architectural improvements.
+#
+
+# More overview
+#
---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+This is your README.
+
+Liz Dennett tutorial for Transform 2020
